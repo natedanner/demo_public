f790362c256763f3a7fc78413a51c47e5cb23df3
Greg Adams <greg@moderne.io>
2021-10-17 09:20:25 -0700

    initial

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean("stupid")
+	public ObjectMapper myStupidObjectMapper() {
+		return new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
+	}
+}
